1a12d440292efb9b6224881fdc0a86f6b9c23067
PouyaKhDev <pouyakhajavidev@gmail.com>
2026-07-06 21:51:50 +0330

    Prepared for deplpyment

--- festival_app/core/settings.py
+++ festival_app/core/settings.py
@@ -19,6 +19,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
 
 
 # Application definition
